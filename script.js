739dd5e3c6dc109f931dc53c800ecd1c7697dfe8
IlonaArt <ilonasurdream@gmail.com>
2019-08-18 21:51:36 +0300

    Finish js

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 (function() {
+    let popUp = document.querySelector('.pop-up');
+    let showPopUpBtn = document.querySelector('.btn-show');
+    let closePopUpBtn = document.querySelector('.header__button-close'); 
     const messageForm = document.getElementById('message-form');
+    const messageTextInput = document.getElementById('message');
     let dialogueContainer = document.querySelector('.dialogue');
-    dialogueContainer.scrollTop = dialogueContainer.offsetHeight;
+
+    showPopUpBtn.addEventListener('click', function(e) {
+        popUp.classList.remove('hidden');
+        event.target.classList.add('hidden');
+        dialogueContainer.scrollTop = dialogueContainer.scrollHeight - dialogueContainer.offsetHeight;
+    });
+
+    closePopUpBtn.addEventListener('click', function() {
+        popUp.classList.add('hidden');
+        showPopUpBtn.classList.remove('hidden');
+    }); 
     
     messageForm.addEventListener('submit', function(e) {
         e.preventDefault();
-        const messageTextInput = document.getElementById('message');
-        let messageTextValue = messageTextInput.value;
+        let messageTextValue = messageTextInput.value.trim();
         if (messageTextValue !== '') {        
             let dialogueMessage = document.createElement('div');
-            dialogueMessage.classList.add('dialogue__item', 'dialogue__item_owner', 'dialogue-message', 'highlighter');
+            dialogueMessage.classList.add('dialogue__item');
+            dialogueMessage.classList.add('dialogue-message'); 
+            dialogueMessage.classList.add('dialogue-message_owner');
+            dialogueMessage.classList.add('highlighter');
             let userImg = document.createElement('img');
             userImg.classList.add('dialogue-message__user-img');
             userImg.setAttribute('src', 'img/img2.jpg');
@@ -25,11 +41,24 @@
             dialogueMessage.appendChild(userText);
             dialogueMessage.appendChild(messageDate);
             dialogueContainer.appendChild(dialogueMessage);
-            dialogueContainer.scrollTop = dialogueContainer.offsetHeight;
-            messageTextInput.value = '';
+            dialogueContainer.scrollTop = dialogueContainer.scrollHeight - dialogueContainer.offsetHeight;
+            messageTextInput.setAttribute('rows', '4');
             setTimeout(function() {
                 dialogueMessage.classList.remove('highlighter');
             }, 2000);
-        }    
-    })
+        }
+        messageTextInput.value = '';    
+    });
+
+    messageTextInput.addEventListener('input', function(event) {
+        const input = event.target;
+        let rows = input.getAttribute('rows');
+        if (input.scrollHeight > input.offsetHeight) {
+            ++rows;
+            if (rows <= 7) {
+                input.setAttribute('rows', rows);
+                dialogueContainer.scrollTop = dialogueContainer.scrollHeight - dialogueContainer.offsetHeight;
+            }
+        }
+    });
 })();
